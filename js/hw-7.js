bd0ccd43e402753766648e3a9e5258d014c9e659
Sergey Matveev <serega-matveef@yandex.ru>
2023-04-26 22:40:08 +0300

    complete 10items

--- js/hw-7.js
+++ js/hw-7.js
@@ -1,4 +1,7 @@
 "use strict";
+let userAnswer, userAnswer2, currentDate;
+
+
 alert('Сейчас начнут выполняться задания');
 
 
@@ -67,14 +70,109 @@ const searchStrStartKeyWord = (array, key) => {
 }
 
 //---Выполнение задания
-console.log('Задание ');
-userStr = String(prompt('Введите массив строк, разделяя их запятой и пробелом после нее'));
+console.log('Задание 2');
+// userStr = String(prompt('Введите массив строк, разделяя их запятой и пробелом после нее'));
 let userArray = userStr.split(', ');
 
-console.log(`Исходный массив: \n[${userArray}]`);
-let keyWord = prompt('Введите ключевую строку для поиска');
-console.log(`Ключевое слово для поиска: \n[${keyWord}]`);
+// console.log(`Исходный массив: \n[${userArray}]`);
+// let keyWord = prompt('Введите ключевую строку для поиска');
+// console.log(`Ключевое слово для поиска: \n[${keyWord}]`);
 
-console.log(`Результат: \n[${searchStrStartKeyWord(userArray, keyWord)}]`);
+// console.log(`Результат: \n[${searchStrStartKeyWord(userArray, keyWord)}]`);
+
+/*---Задание 3---*/
+
+console.log('Задание 3');
+const value = 32.58884;
+console.log(`Округлим ${value} до меньшего целого: ${Math.floor(value)}`);
+console.log(`Округлим ${value} до большего целого: ${Math.ceil(value)}`);
+console.log(`Округлим ${value} до ближайшего целого: ${Math.round(value)}`);
+
+/*---Задание 4---*/
+console.log('Задание 4');
+userArray = [52, 53, 49, 77, 21, 32];
+console.log(`Исходный массив: \n[${userArray}]`);
+
+function getMaxOfArray(numArray) {
+    return Math.max.apply(null, numArray);
+}
+
+console.log(`Максимальный элемент в данном массиве: ${getMaxOfArray(userArray)}`);
+console.log(getMaxOfArray(userArray));
+
+/*---Задание 5---*/
+console.log('Задание 5');
+console.log('число наугад от 1 до 10:');
+console.log(`${Math.round(Math.random() * (10 - 1) + 1)}`);
+
+/*-смотрим распределение-*/
+
+// const viewRasp = (sizeOfArray) => {
+//     const arrayOfView = [];
+//     for (let i = 0; i < sizeOfArray; i++) {
+//         arrayOfView[i] = Math.round(Math.random() * (10 - 1) + 1);
+//     }
+//     return arrayOfView;
+// }
+// userAnswer = Number(prompt('Введите требуемый обьем выборки'));
+// console.log(`В результате такого распределения получим выборку из ${userAnswer} элементов:`);
+// console.log(viewRasp(userAnswer));
+
+/*---Задание 6---*/
+
+
+const viewRasp = (sizeOfArray) => {
+    const arrayOfView = [];
+    for (let i = 0; i < Math.floor(sizeOfArray / 2); i++) {
+        arrayOfView[i] = Math.round(Math.random() * sizeOfArray);
+    }
+    return arrayOfView;
+}
+
+// console.log('Задание 6');
+// userAnswer = inputNumbers(0,100);
+// console.log(`В результате выполнения подпрограммы получим массив из ${Math.floor(userAnswer / 2)} случайных чисел от 0 до ${userAnswer}:`);
+// console.log(viewRasp(userAnswer));
+
+/*---Задание 7---*/
+
+
+const getRandomNumber = (value1, value2) => {
+    const minValue = Math.max(value1, value2);
+    const maxValue = Math.min(value1, value2);
+    return Math.round(Math.random() * (maxValue - minValue) + minValue);
+}
+
+// console.log('Задание 7');
+// userAnswer = inputNumbers(0, 1000);
+// userAnswer2 = inputNumbers(0, 1000);
+// console.log(`В результате выполнения подпрограммы получим случайное число от ${Math.min(userAnswer, userAnswer2)} до ${Math.max(userAnswer, userAnswer2) }:`);
+// console.log(getRandomNumber(userAnswer, userAnswer2));
+
+/*---Задание 8---*/
+
+console.log(`Задание 8`);
+currentDate = new Date();
+console.log(`Текущая дата: \n${currentDate}`);
+
+/*---Задание 9---*/
+console.log(`Задание 9`);
+currentDate.setDate(currentDate.getDate() + 73);
+console.log(`Через 73 дня будет: \n${currentDate}`);
+
+/*---Задание 10---*/
+
+const formatDate = (dateObject) => {
+
+    const outDate = 'Дата: ' + dateObject.getDate() + ' ' +
+        dateObject.toLocaleDateString('ru-RU', { month: 'long' }) + ' ' +
+        dateObject.getFullYear() + ' - это ' + dateObject.toLocaleDateString('ru-RU', { weekday: 'long' }) + '.' +
+        '\nВремя: ' + dateObject.getHours()+':'+dateObject.getMinutes()+':'+dateObject.getSeconds();
+    return outDate;
+}
+
+console.log(`Задание 10`);
+currentDate = new Date();
+console.log(formatDate(currentDate));
 
 
